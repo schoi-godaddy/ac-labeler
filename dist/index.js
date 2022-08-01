ceb8923d8220aeaed993d07d0dd05ec6ea31a67f
Shawn Choi <schoi@godaddy.com>
2022-07-31 20:53:00 -0700

    ref

--- dist/index.js
+++ dist/index.js
@@ -8853,6 +8853,29 @@ const src_1 = __importDefault(__nccwpck_require__(4847));
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8862,23 +8885,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(3934));
-const github_1 = __importDefault(__nccwpck_require__(76));
+const core = __importStar(__nccwpck_require__(3934));
+const github = __importStar(__nccwpck_require__(76));
 const run = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const labelName = core_1.default.getInput("label");
+        const labelName = core.getInput("label");
         console.log(labelName);
         const time = new Date().toTimeString();
-        core_1.default.setOutput("time", time);
+        core.setOutput("time", time);
         if (!process.env.GITHUB_TOKEN) {
             throw new Error("GITHUB_TOKEN environment variable not found.");
         }
-        const client = github_1.default.getOctokit(process.env.GITHUB_TOKEN);
-        const contextPullRequest = github_1.default.context.payload.pull_request;
+        const client = github.getOctokit(process.env.GITHUB_TOKEN);
+        const contextPullRequest = github.context.payload.pull_request;
         if (!contextPullRequest) {
             throw new Error("This action can only be invoked in `pull_request_target` or `pull_request` events.");
         }
@@ -8887,7 +8907,7 @@ const run = () => __awaiter(void 0, void 0, void 0, function* () {
         console.log(contextPullRequest);
     }
     catch (error) {
-        core_1.default.setFailed(error);
+        core.setFailed(error);
     }
 });
 exports["default"] = run;
